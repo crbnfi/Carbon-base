059cca8c046b9bc5a5ba695bdc43a28a03de054a
Isko <isko@carbon.fi>
2014-03-04 22:22:54 +0200

    Vertically centred header element

--- assets/js/plugins.dev.js
+++ assets/js/plugins.dev.js
@@ -1,45 +1,45 @@
-/*! Normalized address bar hiding for iOS & Android (c) @scottjehl MIT License
-	https://github.com/scottjehl/Hide-Address-Bar
-*/
-(function( win ){
+// Normalized address bar hiding for iOS & Android (c) @scottjehl MIT License
+// https://github.com/scottjehl/Hide-Address-Bar
+(function (win) {
 	var doc = win.document;
 
 	// If there's a hash, or addEventListener is undefined, stop here
-	if( !location.hash && win.addEventListener ){
+	if (!location.hash && win.addEventListener) {
 
 		//scroll to 1
-		window.scrollTo( 0, 1 );
+		window.scrollTo(0, 1);
 		var scrollTop = 1,
-			getScrollTop = function(){
+			getScrollTop = function () {
 				return win.pageYOffset || doc.compatMode === "CSS1Compat" && doc.documentElement.scrollTop || doc.body.scrollTop || 0;
 			},
 
 			//reset to 0 on bodyready, if needed
-			bodycheck = setInterval(function(){
-				if( doc.body ){
-					clearInterval( bodycheck );
+			bodycheck = setInterval(function () {
+				if (doc.body) {
+					clearInterval(bodycheck);
 					scrollTop = getScrollTop();
-					win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
+					win.scrollTo(0, scrollTop === 1 ? 0 : 1);
 				}
-			}, 15 );
+			}, 15);
 
-		win.addEventListener( "load", function(){
-			setTimeout(function(){
+		win.addEventListener("load", function () {
+			setTimeout(function () {
 				//at load, if user hasn't scrolled more than 20 or so...
-				if( getScrollTop() < 20 ){
+				if (getScrollTop() < 20) {
 					//reset to hide addr bar at onload
-					win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
+					win.scrollTo(0, scrollTop === 1 ? 0 : 1);
 				}
 			}, 0);
-		} );
+		});
 	}
-})( this );
+})(this);
+
+
 
 // Improved version of JavaScript fix for the iOS viewport scaling bug. See http://www.blog.highub.com/mobile-2/a-fix-for-iphone-viewport-scale-bug/
 // https://gist.github.com/mathiasbynens/901295
 // By @mathias, @cheeaun and @jdalton
-
-(function(doc) {
+(function (doc) {
 
 	var addEvent = 'addEventListener',
 		type = 'gesturestart',
@@ -54,35 +54,48 @@
 
 	if ((meta = meta[meta.length - 1]) && addEvent in doc) {
 		fix();
-		scales = [.25, 1.6];
+		scales = [0.25, 1.6];
 		doc[addEvent](type, fix, true);
 	}
 
 }(document));
 
+
+
 // iOS Sticky :hover Fix – disable :hover on touch devices
 // http://retrogamecrunch.com/tmp/hover-fix
 // based on https://gist.github.com/4404503
-// via https://twitter.com/javan/status/284873379062890496
-// + https://twitter.com/pennig/status/285790598642946048
-// re http://retrogamecrunch.com/tmp/hover
-if ('createTouch' in document)
-{
-	try
-	{
-		var ignore = /:hover/;
-		for (var i=0; i<document.styleSheets.length; i++)
-		{
+if ('createTouch' in document) {
+	try {
+		var ignore = /:hover/;
+		for (var i = 0; i < document.styleSheets.length; i++) {
 			var sheet = document.styleSheets[i];
-			for (var j=sheet.cssRules.length-1; j>=0; j--)
-			{
+			for (var j = sheet.cssRules.length - 1; j >= 0; j--) {
 				var rule = sheet.cssRules[j];
-				if (rule.type === CSSRule.STYLE_RULE && ignore.test(rule.selectorText))
-				{
+				if (rule.type === CSSRule.STYLE_RULE && ignore.test(rule.selectorText)) {
 					sheet.deleteRule(j);
 				}
 			}
 		}
-	}
-	catch(e){}
+	} catch (e) {}
 }
+
+
+
+// 60fps scrolling using pointer-events: none
+// http://www.thecssninja.com/javascript/pointer-events-60fps
+// You need to add this to your CSS:
+// .disable-hover, .disable-hover * { pointer-events: none !important; }
+var body = document.body,
+	timer;
+
+window.addEventListener('scroll', function () {
+	clearTimeout(timer);
+	if (!body.classList.contains('disable-hover')) {
+		body.classList.add('disable-hover');
+	}
+
+	timer = setTimeout(function () {
+		body.classList.remove('disable-hover');
+	}, 250);
+}, false);
