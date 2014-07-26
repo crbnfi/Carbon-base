0ca42987f7c6d684598bd3a3aa795e5a70f78928
Isko <isko.salminen@gmail.com>
2014-07-26 12:31:47 +0300

    Javascript changes

--- assets/js/plugins.dev.js
+++ assets/js/plugins.dev.js
@@ -36,32 +36,6 @@
 
 
 
-// Improved version of JavaScript fix for the iOS viewport scaling bug. See http://www.blog.highub.com/mobile-2/a-fix-for-iphone-viewport-scale-bug/
-// https://gist.github.com/mathiasbynens/901295
-// By @mathias, @cheeaun and @jdalton
-(function (doc) {
-
-	var addEvent = 'addEventListener',
-		type = 'gesturestart',
-		qsa = 'querySelectorAll',
-		scales = [1, 1],
-		meta = qsa in doc ? doc[qsa]('meta[name=viewport]') : [];
-
-	function fix() {
-		meta.content = 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1];
-		doc.removeEventListener(type, fix, true);
-	}
-
-	if ((meta = meta[meta.length - 1]) && addEvent in doc) {
-		fix();
-		scales = [0.25, 1.6];
-		doc[addEvent](type, fix, true);
-	}
-
-}(document));
-
-
-
 // iOS Sticky :hover Fix – disable :hover on touch devices
 // http://retrogamecrunch.com/tmp/hover-fix
 // based on https://gist.github.com/4404503
@@ -110,8 +84,8 @@ $('.scrollto').click(function(e){
 	e.preventDefault();
 	$('html,body').animate({
 		scrollTop: $($(this).data('scrollto')).offset().top + 'px'
-	}, 800)
-})
+	}, 800);
+});
 
 
 
